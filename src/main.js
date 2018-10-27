08538317d8aae0483dec629adc6d95f900ba11a9
FeYbraNt <rldorado86@gmail.com>
2018-10-28 00:21:06 +0200

    Refactor on UI

--- src/main.js
+++ src/main.js
@@ -7,11 +7,6 @@ const HAND_CHOICES = {
 }
 const GAME_MODES = ["human_vs_cpu", "cpu_vs_cpu"];
 
-const GAME_STARTS = document.querySelector("#game");
-const GAME_RESULTS = document.querySelector("#results");
-const GAME_WINNER = document.querySelector("#results h2");
-const PLAY_AGAIN = document.querySelector("#new-game");
-
 GAME_MODES.forEach((game_mode) => {
     const domNode = document.querySelector(`#start-${game_mode}`);
     domNode.addEventListener('click', startGame.bind(null, game_mode));
@@ -29,23 +24,22 @@ function startGame(game_mode) {
     player2 = new CPUPlayer(HAND_CHOICES);
     game = new Game(HAND_CHOICES, player1, player2);
 
-    GAME_STARTS.style.display = "block";
+    showGame_UI();
 
     if (game_mode === 'cpu_vs_cpu') {
         player1.takeHand(); hightlightHand_UI(player1, 1);
         player2.takeHand(); hightlightHand_UI(player2, 2);
         resolveGame(game);
     }
+
+    // Prepare repeat game button
+    const PLAY_AGAIN = document.querySelector("#new-game");
     PLAY_AGAIN.addEventListener('click', startGame.bind(null, game_mode));
 }
 
 function resolveGame(game) {
     const result = game.gameResult();
-    if (result === 'tie') {
-        GAME_WINNER.innerHTML = "It's a tie!!";
-    } else GAME_WINNER.innerHTML = "The winner is " + result + " !!";
-
-    GAME_RESULTS.style.display = "block";
+    showResults_UI(result);
 }
 
 function humanPlaysHand(hand) {
@@ -58,12 +52,29 @@ function humanPlaysHand(hand) {
 
 /* UI functions */
 
+function showGame_UI() {
+    document.querySelector("#game").style.display = "block";
+}
+
+const RESULTS_UI = document.querySelector("#results"); // Results & Play again button
+
+function showResults_UI(result) {
+    RESULTS_UI.style.display = "block";
+    const GAME_WINNER = document.querySelector("#results h2");
+    if (result === 'tie') {
+        GAME_WINNER.innerHTML = "It's a tie!!";
+    } else GAME_WINNER.innerHTML = "The winner is " + result + " !!";
+}
+
+function hideResults_UI() {
+    RESULTS_UI.style.display = "none";
+}
+
 function hightlightHand_UI(player, number) {
     document.querySelector("#game-player" + number + " ." + player.hand).style.backgroundColor = "orange";
 }
-
 function reset_UI() {
-    GAME_RESULTS.style.display = "none";
+    hideResults_UI();
     Object.keys(HAND_CHOICES).forEach((hand) => {
         const domNodes = document.querySelectorAll(`.${hand}`);
         domNodes.forEach((item) => item.style.backgroundColor = "white");
